8747997d4d18d612583f01474318523c92dcc467
Spaulding Andrew J <SpauldingAndrewJ@JohnDeere.com>
2023-01-12 09:16:52 -0600

    Removed venv and added initial file handling

---
+++ FileHandling/main.py
@@ -0,0 +1,19 @@
+with open("sample.txt") as f:
+    f_contents = f.read()
+    print(f_contents)
+
+    #you can specify the amount of characters read by your code by passing a number through the read() function
+    #change your code to run the first 50 characters of the file
+
+    #f_contents = f.read(20)
+    #print the first 20 charcters and end the string with *
+    #print(f_contents, end = "*")
+
+    #f_contents = f.read(20)
+    #print(f_contents, end = "*")
+
+    #use the seek() function to print the first 20 characters
+    #skip to the 40th character
+    #and print the next 20 characters
+
+
